3c4c6a18ee33930e44f6399dfa3d20e17f90151e
Pauchoook <pauchoook@mail.ru>
2023-06-09 15:46:31 +0300

    add spoller

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,14 @@
 import burger from "./files/burger.js";
 import changeNumber from "./files/change-number.js";
-import * as flsFunctions from "./files/functions.js";
 import headerScroll from "./files/headaer-scroll.js";
 import maps from "./files/maps.js";
 import modal from "./files/modal.js";
 import sliders from "./files/sliders.js";
 import smoothScroll from "./files/smooth-scroll.js";
+import spoller from "./files/spoller.js";
 import switchPages from "./files/switch-pages.js";
 import tab from "./files/tab.js";
 import videos from "./files/videos.js";
-
-flsFunctions.mediaAdaptive();
 
 burger();
 switchPages();
@@ -22,3 +20,4 @@ videos();
 smoothScroll();
 changeNumber();
 modal();
+spoller();
