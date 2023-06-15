e6bd2fd9dcf7f2c91405152d821c0c363dfe215a
Pauchoook <pauchoook@mail.ru>
2023-06-15 13:36:45 +0300

    letters

--- src/js/files/burger.js
+++ src/js/files/burger.js
@@ -2,22 +2,24 @@ export default function burger() {
   const burgerBtn = document.querySelector("#burger-btn");
   const burgerMenu = document.querySelector("#burger-menu");
 
-  burgerBtn.addEventListener("click", (e) => {
-    e.stopPropagation();
+  if (burgerBtn) {
+    burgerBtn.addEventListener("click", (e) => {
+      e.stopPropagation();
 
-    burgerBtn.classList.toggle("active");
-    burgerMenu.classList.toggle("open");
-    document.body.classList.toggle("body-hidden");
+      burgerBtn.classList.toggle("active");
+      burgerMenu.classList.toggle("open");
+      document.body.classList.toggle("body-hidden");
 
-    burgerMenu.addEventListener("click", (e) => e.stopPropagation());
-    document.addEventListener("click", handleDocument);
-  });
+      burgerMenu.addEventListener("click", (e) => e.stopPropagation());
+      document.addEventListener("click", handleDocument);
+    });
 
-  function handleDocument(e) {
-    burgerBtn.classList.remove("active");
-    burgerMenu.classList.remove("open");
-    document.body.classList.remove("body-hidden");
+    function handleDocument(e) {
+      burgerBtn.classList.remove("active");
+      burgerMenu.classList.remove("open");
+      document.body.classList.remove("body-hidden");
 
-    return document.removeEventListener("click", handleDocument);
+      return document.removeEventListener("click", handleDocument);
+    }
   }
 }
--- src/js/files/headaer-scroll.js
+++ src/js/files/headaer-scroll.js
@@ -1,14 +1,17 @@
 export default function headerScroll() {
   const header = document.querySelector(".header");
-  const height = header.clientHeight;
+  
+  if (header) {
+    const height = header.clientHeight;
 
-  window.addEventListener("scroll", (e) => {
-    const currentY = window.scrollY;
+    window.addEventListener("scroll", (e) => {
+      const currentY = window.scrollY;
 
-    if (currentY > height) {
-      header.classList.add("header-scroll");
-    } else {
-      header.classList.remove("header-scroll");
-    }
-  });
+      if (currentY > height) {
+        header.classList.add("header-scroll");
+      } else {
+        header.classList.remove("header-scroll");
+      }
+    });
+  }
 }
